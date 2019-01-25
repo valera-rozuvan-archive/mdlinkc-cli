26f5fe8a71240176d01478954739c71b9a70c5b4
Valera Rozuvan <valera.rozuvan@gmail.com>
2019-01-25 13:20:37 +0200

    Integrating with mdlinkc module.

--- bin/main.js
+++ bin/main.js
@@ -3,9 +3,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_version_1 = require("./app_version");
+const mdlinkc_1 = require("mdlinkc");
 function greeter(msg) {
     return msg;
 }
 const helloMsg = `mdlinkc v${app_version_1.AppVersion.version}`;
 console.log(greeter(helloMsg));
+console.log('');
+mdlinkc_1.Mdlinkc.getLinks().forEach((link, id) => {
+    console.log(`[link ${id}]: ${link}`);
+});
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { AppVersion } from './app_version';
+import { Mdlinkc } from 'mdlinkc';
 
 function greeter(msg: string) {
   return msg;
@@ -7,3 +8,8 @@ function greeter(msg: string) {
 const helloMsg = `mdlinkc v${AppVersion.version}`;
 
 console.log(greeter(helloMsg));
+console.log('');
+
+Mdlinkc.getLinks().forEach((link, id) => {
+  console.log(`[link ${id}]: ${link}`);
+});
