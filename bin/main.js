5d4bb29c596f0d87461ef5893105c2696ce46e13
Valera Rozuvan <valera.rozuvan@gmail.com>
2019-01-25 02:47:36 +0200

    Initial source commit.

---
+++ bin/main.js
@@ -0,0 +1,6 @@
+function greeter(msg) {
+    return msg;
+}
+const helloMsg = 'mdlinkc v0.0.1';
+console.log(greeter(helloMsg));
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+function greeter(msg: string) {
+  return msg;
+}
+
+const helloMsg = 'mdlinkc v0.0.1';
+
+console.log(greeter(helloMsg));
